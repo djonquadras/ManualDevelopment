752e3a9d7d69bc226a6b856f3f43935786f0edbc
Djonathan Quadras <djonquadras@gmail.com>
2025-05-14 13:34:30 +0200

    Update on documentation

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 # main.py
 
-from pylatex import Document, Section, Command, NoEscape, Package, Figure, NewLine, Subsection
-from pylatex.utils import bold
-import parts.configuration as config
+from pylatex import Document, Command, NoEscape, Package
 from utils.build import generate_pdf
 from parts.cover import add_cover
 from parts.about import add_about
-from parts.requirements import add_requirements
 from parts.timetable import add_timetable
 from parts.conclusion import add_conclusion
 from parts.description import add_description
-from parts.generativeAI import makeQuestion
 
+# Create the document
 doc = Document('article')
+
+# Import all the required packages
 doc.packages.append(Package('geometry'))
 doc.packages.append(Package('setspace'))
 doc.packages.append(Package('booktabs'))
@@ -21,20 +20,16 @@ doc.packages.append(Package('graphicx'))
 doc.packages.append(Package('pgfgantt'))
 
 
-
+# Change the summary name
 doc.preamble.append(NoEscape(r'\renewcommand{\contentsname}{Summary}'))
 
-# Adiciona logo ao preâmbulo
-doc.preamble.append(NoEscape(r'\usepackage{graphicx}'))
-
-# Chama a função que adiciona a capa
+# Add the cover
 add_cover(doc)
 
+# Change the page layout
 doc.append(NoEscape(r'\newgeometry{top=3.5cm, bottom=2cm, left=2cm, right=2cm}'))
 
-
-# Personalizar o cabeçalho com imagens e texto centralizado
-# Definição do cabeçalho com imagem e texto
+# Change header
 doc.preamble.append(Command('usepackage', 'fancyhdr'))
 doc.preamble.append(NoEscape(r'\pagestyle{fancy}'))
 doc.preamble.append(NoEscape(r'\setlength{\headwidth}{510pt}'))
@@ -43,21 +38,26 @@ doc.preamble.append(NoEscape(r'\fancyhead[C]{\textbf{Project Proposal}}'))      
 doc.preamble.append(NoEscape(r'\fancyhead[R]{\includegraphics[width=1cm]{unifi-logo.png}}'))  # canto direito
 doc.append(NoEscape(r'\onehalfspacing'))
 doc.append(NoEscape(r'\setlength{\parindent}{12pt}'))
-
 doc.preamble.append(NoEscape(r'\setlength{\headheight}{2cm}'))
 
+# Create the summary
 doc.append(Command('tableofcontents'))
 doc.append(Command('newpage'))
 
+# Add the "About" Section
 add_about(doc)
 doc.append(Command('newpage'))
-add_description(doc)
 
+# Add the "Proposed Model" section
+add_description(doc)
 doc.append(Command('newpage'))
+
+# Add the timetable and activities section
 add_timetable(doc)
 
+# Add the final section
 doc.append(Command('newpage'))
 add_conclusion(doc)
 
-# Gera o PDF
+# Generate PDF
 generate_pdf(doc, 'Results/documento_com_capa', clean_tex=False)
